93342d895b6e557dabaf23a2eeb1e063243367c7
RussCoder <RussCoder@yandex.ru>
2017-08-18 22:02:51 +0300

    Feature: Rendering logic improvement Fast initial rendering on canvas and subsequent conversion of Image Data to data URL and creation of smooth image on img object.

--- djvujs/DjVuViewer.js
+++ djvujs/DjVuViewer.js
@@ -5,20 +5,26 @@ class DjVuViewer {
         this.defaultDPI = 100;
         this.selector = selector;
         this.element = document.querySelector(selector);
-        this.canvas = document.createElement('canvas');
-        this.canvasCtx = this.canvas.getContext('2d');
+        this.fileReader = new FileReader();
+        this.tmpCanvas = document.createElement('canvas');
+        this.tmpCanvasCtx = this.tmpCanvas.getContext('2d');
         this.prevBut = this.element.querySelector('.controls .navbut.prev');
         this.nextBut = this.element.querySelector('.controls .navbut.next');
         this.pageNumberBox = this.element.querySelector('.controls .page_number');
         this.scaleSlider = this.element.querySelector('.controls .scale');
         this.scaleLabel = this.element.querySelector('.controls .scale_label');
         this.img = this.element.querySelector('.image_wrapper img');
+        this.img.style.display = 'none';
+        this.canvas = this.element.querySelector('.image_wrapper canvas');
+        this.canvasCtx = this.canvas.getContext('2d');
         this.imgWrapper = this.element.querySelector('.image_wrapper');
         this._curPage = 0;
         this.pageNumber = null;
         this.stdWidth
         /** @type {DjVuWorker} */
         this.worker = worker || new DjVuWorker();
+
+        this.isCanvasMode = true;
 
         this.element.style.width = window.innerWidth * 0.9 + 'px';
         this.element.style.height = window.innerHeight * 0.9 + 'px';
@@ -31,6 +37,7 @@ class DjVuViewer {
     }
 
     reset() {
+        clearTimeout(this.improveImageTimeout);
         if (this.nextBut) {
             this.nextBut.onclick = null;
             this.prevBut.onclick = null;
@@ -44,7 +51,7 @@ class DjVuViewer {
 
     changeScale() {
         this.scaleLabel.innerText = this.scaleSlider.value;
-        this.img.width = this.stdWidth * (+this.scaleSlider.value / 100);
+        this.isCanvasMode ? this.drawImageOnCanvas() : this.rescaleImageOnImg();
     }
 
     renderEnteredPageByEnter(e) {
@@ -84,9 +91,21 @@ class DjVuViewer {
         this.prevBut.disabled = false;
     }
 
+    getScaledImageWidth() {
+        return this.imageData.width / this.standardScale * (+this.scaleSlider.value / 100);
+    }
+
     renderCurPage() {
+        clearTimeout(this.improveImageTimeout);
         return this.worker.getPageImageDataWithDPI(this._curPage).then(obj => {
-            this.drawImage(obj.imageData, obj.dpi);
+            this.imageData = obj.imageData;
+            this.imageDPI = obj.dpi;
+            this.standardScale = this.imageDPI ? this.imageDPI / Globals.defaultDPI : 1;
+            this.drawImageOnCanvas();
+
+            this.improveImageTimeout = setTimeout(() => {
+                this.drawImageViaImg();
+            }, 1000);
         });
     }
 
@@ -135,15 +154,83 @@ class DjVuViewer {
         });
     }
 
-    drawImage(image, dpi) {
-        var scale = dpi ? dpi / this.defaultDPI : 1;
+    _switchToImageMode() {
+        this.img.style.display = 'block';
+        this.canvas.style.display = 'none';
+        this.isCanvasMode = false;
+    }
 
-        this.canvas.width = image.width;
-        this.canvas.height = image.height;
+    _switchToCanvasMode() {
+        this.img.style.display = 'none';
+        this.canvas.style.display = 'block';
+        this.isCanvasMode = true;
+    }
 
-        this.canvasCtx.putImageData(image, 0, 0);
-        this.img.src = this.canvas.toDataURL();
-        this.stdWidth = image.width / scale;
-        this.img.width = this.stdWidth * (+this.scaleSlider.value / 100);
+    getImageDataURL() {
+        this.tmpCanvas.width = this.imageData.width;
+        this.tmpCanvas.height = this.imageData.height;
+        this.tmpCanvasCtx.putImageData(this.imageData, 0, 0);
+        return this.tmpCanvas.toDataURL();
+    }
+
+    getImageDataURLAsync() {
+        return new Promise(resolve => {
+            this.tmpCanvas.width = this.imageData.width;
+            this.tmpCanvas.height = this.imageData.height;
+            this.tmpCanvasCtx.putImageData(this.imageData, 0, 0);
+            this.tmpCanvas.toBlob(imageBlob => {
+                this.fileReader.onload = event => {
+                    resolve(event.target.result);
+                };
+                this.fileReader.readAsDataURL(imageBlob);
+            });
+        });
+    }
+
+    drawImageViaImg() {
+        this.img.src = this.getImageDataURL();
+        this.rescaleImageOnImg();
+        this._switchToImageMode();
+    }
+
+    rescaleImageOnImg() {
+        this.img.width = this.getScaledImageWidth();
+    }
+
+    drawImageOnCanvas() {
+        //var time = performance.now();
+        var image = this.imageData;
+        var scale = this.imageDPI ? this.imageDPI / Globals.defaultDPI : 1;
+        scale /= (+this.scaleSlider.value / 100)
+        this.stdWidth = image.width / scale * (+this.scaleSlider.value / 100);
+        this.stdHeight = image.height / scale * (+this.scaleSlider.value / 100);
+
+        this.tmpCanvas.width = image.width;
+        this.tmpCanvas.height = image.height;
+        this.tmpCanvasCtx.putImageData(image, 0, 0);
+
+        var tmpH, tmpW, tmpH2, tmpW2;
+        tmpH = tmpH2 = this.tmpCanvas.height;
+        tmpW = tmpW2 = this.tmpCanvas.width;
+
+        if (scale > 4) {
+            tmpH = this.tmpCanvas.height / scale * 4;
+            tmpW = this.tmpCanvas.width / scale * 4;
+            //первое сжатие
+            this.tmpCanvasCtx.drawImage(this.tmpCanvas, 0, 0, tmpW, tmpH);
+        }
+        if (scale > 2) {
+            tmpH2 = this.tmpCanvas.height / scale * 2;
+            tmpW2 = this.tmpCanvas.width / scale * 2;
+            //второе сжатие
+            this.tmpCanvasCtx.drawImage(this.tmpCanvas, 0, 0, tmpW, tmpH, 0, 0, tmpW2, tmpH2);
+        }
+        //итоговое сжатие
+        this.canvas.width = image.width / scale;
+        this.canvas.height = image.height / scale;
+        this.canvasCtx.drawImage(this.tmpCanvas, 0, 0, tmpW2, tmpH2,
+            0, 0, this.canvas.width, this.canvas.height);
+        this._switchToCanvasMode();
+        //console.log('Render time', performance.now() - time, scale);
     }
 }
