d603d15ecb13c82e37cbbae1730bbf457a81228a
RussCoder <RussCoder@yandex.ru>
2018-02-05 16:16:41 +0300

    Viewer update

--- dist/djvu.js
+++ dist/djvu.js
@@ -7,11 +7,14 @@ var DjVu = {
 };
 DjVu.Utils = {
     loadFile(url, responseType = 'arraybuffer') {
-        return new Promise(resolve => {
+        return new Promise((resolve, reject) => {
             var xhr = new XMLHttpRequest();
             xhr.open("GET", url);
             xhr.responseType = responseType;
             xhr.onload = (e) => {
+                if (xhr.status !== 200) {
+                    return reject({ message: "Something went wrong!", xhr: xhr })
+                }
                 DjVu.IS_DEBUG && console.log("File loaded: ", e.loaded);
                 resolve(xhr.response);
             };
