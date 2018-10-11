3b8e37d6af8e31d7cf3bb1f50c7668218ee3253a
RussCoder <RussCoder@yandex.ru>
2018-10-11 19:13:24 +0300

    E:: A workaround for Chrome GPU memory leakage Now the extension is reloaded when the last tab is closed. And some minor improvements.

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,4 @@
-chrome.browserAction.onClicked.addListener(() => chrome.tabs.create({ url: "viewer.html" }));
+'use strict';
 
 chrome.contextMenus.create({
     id: "open_with",
@@ -18,9 +18,9 @@ function promisify(func) {
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (sender.tab && message === "include_scripts") {
         Promise.all([
-            promisify(chrome.tabs.executeScript)(sender.tab.id, { file: 'djvu.js' }),
-            promisify(chrome.tabs.executeScript)(sender.tab.id, { file: 'djvu_viewer.js' }),
-            promisify(chrome.tabs.insertCSS)(sender.tab.id, { file: 'djvu_viewer.css' })
+            promisify(chrome.tabs.executeScript)(sender.tab.id, { file: 'djvu.js', runAt: "document_end" }),
+            promisify(chrome.tabs.executeScript)(sender.tab.id, { file: 'djvu_viewer.js', runAt: "document_end" }),
+            promisify(chrome.tabs.insertCSS)(sender.tab.id, { file: 'djvu_viewer.css', runAt: "document_end" })
         ]).then(() => {
             sendResponse();
         })
@@ -28,8 +28,23 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     }
 })
 
+var tabIds = new Set();
+
+chrome.tabs.onRemoved.addListener(tabId => { // to bypass a Chrome bug due to which GPU memory is not released after all tabs are closed.
+    if (tabIds.has(tabId)) {
+        tabIds.delete(tabId);
+        if (!tabIds.size) {
+            chrome.runtime.reload();
+        }
+    }
+});
+
+chrome.browserAction.onClicked.addListener(() => {
+    chrome.tabs.create({ url: "viewer.html" }, tab => tabIds.add(tab.id));
+});
+
 chrome.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === "open_with") {
-        chrome.tabs.create({ url: "viewer.html" + "#" + info.linkUrl });
+        chrome.tabs.create({ url: "viewer.html" + "#" + info.linkUrl }, tab => tabIds.add(tab.id));
     }
 });
