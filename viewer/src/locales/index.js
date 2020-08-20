63826af39d401f60e6b77415d9523da9c6e16f36
Remigijus Pankevičius <r-pankevicius@users.noreply.github.com>
2020-08-20 15:43:14 +0300

    Added Swedish translations

--- viewer/src/locales/index.js
+++ viewer/src/locales/index.js
@@ -1,7 +1,9 @@
 import English from './English';
 import Russian from './Russian';
+import Swedish from './Swedish';
 
 export default {
     'en': English,
     'ru': Russian,
+    'sv': Swedish
 };
